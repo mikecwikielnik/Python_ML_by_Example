145a97fb2fd72fb5f3271b318e09f01abe9da331
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-06-22 16:40:00 -0400

    Add files via upload

--- Ch 4/decision_tree_ch_4.py
+++ Ch 4/decision_tree_ch_4.py
@@ -267,3 +267,36 @@ def visualize_tree(node, depth=0):
         print(f"{depth * ' '}[{node}]")
 
 visualize_tree(tree)
+
+# test with a numerical example
+
+X_train_n = [[6, 7],
+             [2, 4],
+             [7, 2],
+             [3, 6],
+             [4, 7],
+             [5, 2],
+             [1, 6],
+             [2, 0],
+             [6, 3],
+             [4, 1]]
+
+y_train_n = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
+
+tree = train_tree(X_train_n, y_train_n, 2, 2)
+
+visualize_tree(tree)
+
+# implementing a decision tree with scikit-learn
+
+from sklearn.tree import DecisionTreeClassifier
+tree_sk = DecisionTreeClassifier(criterion='gini',
+                                 max_depth=2, min_samples_split=2)
+tree_sk.fit(X_train_n, y_train_n)
+
+# to visualize the tree just built
+
+from sklearn.tree import export_graphviz
+export_graphviz(tree_sk, out_file='tree.dot',
+                feature_names=['X1', 'X2'], impurity=False,
+                filled=True, class_names=['0', '1'])
