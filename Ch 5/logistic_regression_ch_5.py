243b83d24557689df04ac66ee71c1a004504c927
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-02 16:38:07 -0400

    Add files via upload

--- Ch 5/logistic_regression_ch_5.py
+++ Ch 5/logistic_regression_ch_5.py
@@ -9,3 +9,18 @@ import numpy as np
 def sigmoid(input):
     return 1.0 / (1 + np.exp(-input))
 
+# input values are -8 to 8, & the output is as follows:
+
+import matplotlib.pyplot as plt
+z = np.linspace(-8, 8, 1000)
+y = sigmoid(z)
+plt.plot(z, y)
+plt.axhline(y = 0, ls = 'dotted', color = 'k')
+plt.axhline(y = 0.5, ls = 'dotted', color = 'k')
+plt.axhline(y = 1, ls = 'dotted', color = 'k')
+plt.yticks([0.0, 0.25, 0.5, 0.75, 1.0])
+plt.xlabel('z')
+plt.ylabel('y(z)')
+plt.show()
+
+
