953fff2fc9776cccd3e5eaa3e01a1d6ef508d7be
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-08 14:05:41 -0400

    Add files via upload

--- Ch 5/logistic_regression_from_scratch_ch_5.py
+++ Ch 5/logistic_regression_from_scratch_ch_5.py
@@ -123,3 +123,32 @@ plt.scatter(X_test[:, 0], X_test[:, 1], marker='*', c=colours)
 plt.xlabel('x1')
 plt.xlabel('x2')
 plt.show()
+
+# Predicting ad click-through with logistic regression using gradient descent
+
+# Liu, Yuxi (Hayden). Python Machine Learning By Example: Build intelligent systems using Python, TensorFlow 2, PyTorch, and scikit-learn, 3rd Edition (p. 165). Packt Publishing. Kindle Edition. 
+
+"""
+After a brief example, we will now deploy the algo we just developed in
+our click-through prediction project
+"""
+
+import pandas as pd
+
+n_rows = 300000
+df = pd.read_csv("train.csv", nrows=n_rows)
+
+X = df.drop(['click', 'id', 'hour', 'device_id', 'device_ip'], axis=1).values
+Y = df['click'].values
+
+n_train = 100000
+X_train = X[:n_train]
+Y_train = Y[:n_train]
+X_test = X[n_train:]
+Y_test = Y[n_train:]
+
+from sklearn.preprocessing import OneHotEncoder
+enc = OneHotEncoder(handle_unknown='ignore')
+X_train_enc = enc.fit_transform(X_train)
+
+X_test_enc = enc.transform(X_test)
