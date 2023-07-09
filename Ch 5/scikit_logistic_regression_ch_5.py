6550dab85afe8a04cf7b9d167b46534ab885b16b
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-08 23:12:18 -0400

    Add files via upload

---
+++ Ch 5/scikit_logistic_regression_ch_5.py
@@ -0,0 +1,24 @@
+"""
+SGDClassifier module of scikit-learn:
+
+Liu, Yuxi (Hayden). Python Machine Learning By Example: Build intelligent systems using Python, TensorFlow 2, PyTorch, and scikit-learn, 3rd Edition (p. 168). Packt Publishing. Kindle Edition. 
+"""
+
+import pandas as pd
+n_rows = 300000
+df = pd.read_csv("train", nrows=n_rows)
+
+X = df.drop(['click', 'id', 'hour', 'device_id', 'device_ip'], axis=1).values
+Y = df['click'].values
+
+n_train = 100000
+X_train = X[:n_train]
+Y_train = Y[:n_train]
+X_test = X[n_train:]
+Y_test = Y[n_train:]
+
+from sklearn.preprocessing import OneHotEncoder
+enc = OneHotEncoder(handle_unknown='ignore')
+X_train_enc = enc.fit_transform(X_train)
+
+X_test_enc = enc.transform(X_test)
