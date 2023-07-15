636918ce3bc01878d01bdd2a0099b158723c9714
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-14 23:26:22 -0400

    Add files via upload

--- Ch 7/get_dji_data_ch_7.py
+++ Ch 7/get_dji_data_ch_7.py
@@ -10,3 +10,19 @@ import yfinance as yf
 djia_data = yf.download("^DJi", start="2005-12-01", end="2005-12-10", group_by='tickers')
 djia_data
 
+def generate_features(df):
+    """
+    Generate features for a stock/index based on historical price and performance
+    @param df: dataframe with columns "open", "close", "high", "low", "volume", "adjusted close"
+    @return: dataframe, data set with new features
+    """
+    df_new = pd.DataFrame()
+    # 6 original features
+    df_new['open'] = df['Open']
+    df_new['open_1'] = df['Open'].shift(1)
+    df_new['close_1'] = df['Close'].shift(1)
+    df_new['high_1'] = df['High'].shift(1)
+    df_new['low_1'] = df['Low'].shift(1)
+    df_new['volume_1'] = df['Volume'].shift(1)
+
+    
