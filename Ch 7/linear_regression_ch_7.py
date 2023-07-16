6515f69f29f84089605761288daf6c7752e4c1eb
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-16 19:47:55 -0400

    Add files via upload

--- Ch 7/linear_regression_ch_7.py
+++ Ch 7/linear_regression_ch_7.py
@@ -14,3 +14,14 @@ def compute_prediction(X, weights):
     predictions = np.dot(X, weights)
     return predictions
 
+def update_weights_gd(X_train, y_train, weights, learning_rate):
+    """
+    Update weights by one step and return updated weights
+    """
+
+    predictions = compute_prediction(X_train, weights)
+    weights_delta = np.dot(X_train.T, y_train - predictions)
+    m = y_train.shape[0]
+    weights += learning_rate / float(m) * weights_delta
+    return weights
+
