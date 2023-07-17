d4cd73c100b2687fb75ca3f743cf39ee0dabf11b
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-16 20:42:21 -0400

    Add files via upload

--- Ch 7/linear_regression_ch_7.py
+++ Ch 7/linear_regression_ch_7.py
@@ -111,3 +111,9 @@ predictions = predict(X_test, weights)
 print(predictions)
 print(y_test)
 
+# Directly use SGDRegressor from scikit-learn 
+
+from sklearn.linear_model import SGDRegressor
+regressor = SGDRegressor(loss='squared_loss', penalty='l2', alpha=0.0001, learning_rate='constant', eta=0.01,
+                        max_iter=1000)
+
