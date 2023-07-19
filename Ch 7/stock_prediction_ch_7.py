b2f89b7521cba6d452c614dba6b4b5d40d3e295c
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-19 12:55:51 -0400

    Add files via upload

--- Ch 7/stock_prediction_ch_7.py
+++ Ch 7/stock_prediction_ch_7.py
@@ -1,22 +1,19 @@
 """
-Predicting Stock Prices with Regression Algorithms
+Predicting stock prices with the three regression algorithms
 
-Liu, Yuxi (Hayden). Python Machine Learning By Example: Build intelligent systems using Python, TensorFlow 2, PyTorch, and scikit-learn, 3rd Edition (p. 213). Packt Publishing. Kindle Edition.
+Liu, Yuxi (Hayden). Python Machine Learning By Example: Build intelligent systems using Python, TensorFlow 2, PyTorch, and scikit-learn, 3rd Edition (p. 246). Packt Publishing. Kindle Edition. 
 """
 
 import pandas as pd
-import yfinance as yf
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.preprocessing import StandardScaler
 
-djia_data = yf.download("^DJi", start="2005-12-01", end="2005-12-10", group_by='tickers')
-djia_data.head()
-djia_data.index
-djia_data.dtypes
-list(djia_data.index)
 
 def generate_features(df):
     """
     Generate features for a stock/index based on historical price and performance
-    @param df: dataframe with columns "open", "close", "high", "low", "volume", "adjusted close"
+    @param df: dataframe with columns "Open", "Close", "High", "Low", "Volume", "Adjusted Close"
     @return: dataframe, data set with new features
     """
     df_new = pd.DataFrame()
@@ -27,7 +24,6 @@ def generate_features(df):
     df_new['high_1'] = df['High'].shift(1)
     df_new['low_1'] = df['Low'].shift(1)
     df_new['volume_1'] = df['Volume'].shift(1)
-
     # 31 generated features
     # average price
     df_new['avg_price_5'] = df['Close'].rolling(5).mean().shift(1)
@@ -36,7 +32,6 @@ def generate_features(df):
     df_new['ratio_avg_price_5_30'] = df_new['avg_price_5'] / df_new['avg_price_30']
     df_new['ratio_avg_price_5_365'] = df_new['avg_price_5'] / df_new['avg_price_365']
     df_new['ratio_avg_price_30_365'] = df_new['avg_price_30'] / df_new['avg_price_365']
-    
     # average volume
     df_new['avg_volume_5'] = df['Volume'].rolling(5).mean().shift(1)
     df_new['avg_volume_30'] = df['Volume'].rolling(21).mean().shift(1)
@@ -44,7 +39,6 @@ def generate_features(df):
     df_new['ratio_avg_volume_5_30'] = df_new['avg_volume_5'] / df_new['avg_volume_30']
     df_new['ratio_avg_volume_5_365'] = df_new['avg_volume_5'] / df_new['avg_volume_365']
     df_new['ratio_avg_volume_30_365'] = df_new['avg_volume_30'] / df_new['avg_volume_365']
-
     # standard deviation of prices
     df_new['std_price_5'] = df['Close'].rolling(5).std().shift(1)
     df_new['std_price_30'] = df['Close'].rolling(21).std().shift(1)
@@ -52,35 +46,32 @@ def generate_features(df):
     df_new['ratio_std_price_5_30'] = df_new['std_price_5'] / df_new['std_price_30']
     df_new['ratio_std_price_5_365'] = df_new['std_price_5'] / df_new['std_price_365']
     df_new['ratio_std_price_30_365'] = df_new['std_price_30'] / df_new['std_price_365']
-
-    # standard deviation of volumes 
+    # standard deviation of volumes
     df_new['std_volume_5'] = df['Volume'].rolling(5).std().shift(1)
     df_new['std_volume_30'] = df['Volume'].rolling(21).std().shift(1)
     df_new['std_volume_365'] = df['Volume'].rolling(252).std().shift(1)
     df_new['ratio_std_volume_5_30'] = df_new['std_volume_5'] / df_new['std_volume_30']
     df_new['ratio_std_volume_5_365'] = df_new['std_volume_5'] / df_new['std_volume_365']
     df_new['ratio_std_volume_30_365'] = df_new['std_volume_30'] / df_new['std_volume_365']
-
-    # return
+    # # return
     df_new['return_1'] = ((df['Close'] - df['Close'].shift(1)) / df['Close'].shift(1)).shift(1)
-    df_new['return_5'] = ((df['Close'] - df['Close'].shift(1)) / df['Close'].shift(1)).shift(1)
-    df_new['return_30'] = ((df['Close'] - df['Close'].shift(1)) / df['Close'].shift(1)).shift(1)
-    df_new['return_365'] = ((df['Close'] - df['Close'].shift(1)) / df['Close'].shift(1)).shift(1)
+    df_new['return_5'] = ((df['Close'] - df['Close'].shift(5)) / df['Close'].shift(5)).shift(1)
+    df_new['return_30'] = ((df['Close'] - df['Close'].shift(21)) / df['Close'].shift(21)).shift(1)
+    df_new['return_365'] = ((df['Close'] - df['Close'].shift(252)) / df['Close'].shift(252)).shift(1)
     df_new['moving_avg_5'] = df_new['return_1'].rolling(5).mean().shift(1)
     df_new['moving_avg_30'] = df_new['return_1'].rolling(21).mean().shift(1)
     df_new['moving_avg_365'] = df_new['return_1'].rolling(252).mean().shift(1)
-
-    # target
+    # the target
     df_new['close'] = df['Close']
     df_new = df_new.dropna(axis=0)
     return df_new
 
-data = generate_features(djia_data)
-print(data)
-      
-djia_data.to_csv("20051201_20051210.csv")
+data_raw = pd.csv('19880101_20191231.csv', index_col='Date')    # this dataset does not exist
+data = generate_features(data_raw)
 
-fake_data = pd.read_csv('20051201_20051210.csv', index_col='Date')
-data = generate_features(fake_data)
-data
-fake_data.dtypes
+start_train = '1988-01-01'
+end_train = '2018-12-31'
+
+start_test = '2019-01-01'
+end_test = '2019-12-31'
+
