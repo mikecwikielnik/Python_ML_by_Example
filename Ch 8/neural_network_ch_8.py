aeafd1c0d9db4d682a7f4b2c71884f7a4e9e20a7
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-19 18:15:59 -0400

    Add files via upload

--- Ch 8/neural_network_ch_8.py
+++ Ch 8/neural_network_ch_8.py
@@ -18,3 +18,39 @@ def sigmoid(z):
 def sigmoid_derivative(z):
     return sigmoid(z) * (1.0 - sigmoid(z))
 
+# define the training fn, which takes the training dataset, 
+# the number of units in the hidden layer (we use 1 hidden layer as ex)
+# and the number of iterations
+
+def train(X, y, n_hidden, learning_rate, n_iter):
+    m, n_input = X.shape
+    W1 = np.random.randn(n_input, n_hidden)
+    b1 = np.zeros((1, n_hidden))
+    W2 = np.random.randn(n_hidden, 1)
+    b2 = np.zeros((1, 1))
+    for i in range(1, n_iter + 1):
+        Z2 = np.matmul(X, W1) + b1
+        A2 = sigmoid(Z2)
+        Z3 = np.matmul(A2, W2) + b2
+        A3 = Z3
+
+        dZ3 = A3 - y
+        dW2 = np.matmul(A2.T, dZ3)
+        db2 = np.sum(dZ3, axis=0, keepdims=True)
+
+        dZ2 = np.matmul(dZ3, W2.T) * sigmoid_derivative(Z2)
+        dW1 = np.matmul(X.T, dZ2)
+        db1 = np.sum(dZ2, axis=0)
+
+        W2 = W2 - learning_rate * dW2 / m
+        b2 = b2 - learning_rate * db2 / m
+        W1 = W1 - learning_rate * dW1 / m
+        b1 = b1 - learning_rate * db1 / m
+
+        if i % 100 == 0:
+            cost = np.mean((y - A3) ** 2)
+            print('Iteration %i, training loss: %f' % (i, cost))
+
+    model = {'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
+    return model
+
