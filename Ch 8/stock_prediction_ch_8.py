6fcaae4cb07b79ae9756264fda227aedad45b3f0
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-20 16:00:02 -0400

    Add files via upload

---
+++ Ch 8/stock_prediction_ch_8.py
@@ -0,0 +1,86 @@
+"""
+Predicting stock prices with neural networks
+
+Liu, Yuxi (Hayden). Python Machine Learning By Example: Build intelligent systems using Python, TensorFlow 2, PyTorch, and scikit-learn, 3rd Edition (p. 271). Packt Publishing. Kindle Edition. 
+"""
+
+
+import pandas as pd
+
+
+def generate_features(df):
+    """
+    Generate features for a stock/index based on historical price and performance
+    @param df: dataframe with columns "Open", "Close", "High", "Low", "Volume", "Adjusted Close"
+    @return: dataframe, data set with new features
+    """
+    df_new = pd.DataFrame()
+    # 6 original features
+    df_new['open'] = df['Open']
+    df_new['open_1'] = df['Open'].shift(1)
+    df_new['close_1'] = df['Close'].shift(1)
+    df_new['high_1'] = df['High'].shift(1)
+    df_new['low_1'] = df['Low'].shift(1)
+    df_new['volume_1'] = df['Volume'].shift(1)
+    # 31 generated features
+    # average price
+    df_new['avg_price_5'] = df['Close'].rolling(5).mean().shift(1)
+    df_new['avg_price_30'] = df['Close'].rolling(21).mean().shift(1)
+    df_new['avg_price_365'] = df['Close'].rolling(252).mean().shift(1)
+    df_new['ratio_avg_price_5_30'] = df_new['avg_price_5'] / df_new['avg_price_30']
+    df_new['ratio_avg_price_5_365'] = df_new['avg_price_5'] / df_new['avg_price_365']
+    df_new['ratio_avg_price_30_365'] = df_new['avg_price_30'] / df_new['avg_price_365']
+    # average volume
+    df_new['avg_volume_5'] = df['Volume'].rolling(5).mean().shift(1)
+    df_new['avg_volume_30'] = df['Volume'].rolling(21).mean().shift(1)
+    df_new['avg_volume_365'] = df['Volume'].rolling(252).mean().shift(1)
+    df_new['ratio_avg_volume_5_30'] = df_new['avg_volume_5'] / df_new['avg_volume_30']
+    df_new['ratio_avg_volume_5_365'] = df_new['avg_volume_5'] / df_new['avg_volume_365']
+    df_new['ratio_avg_volume_30_365'] = df_new['avg_volume_30'] / df_new['avg_volume_365']
+    # standard deviation of prices
+    df_new['std_price_5'] = df['Close'].rolling(5).std().shift(1)
+    df_new['std_price_30'] = df['Close'].rolling(21).std().shift(1)
+    df_new['std_price_365'] = df['Close'].rolling(252).std().shift(1)
+    df_new['ratio_std_price_5_30'] = df_new['std_price_5'] / df_new['std_price_30']
+    df_new['ratio_std_price_5_365'] = df_new['std_price_5'] / df_new['std_price_365']
+    df_new['ratio_std_price_30_365'] = df_new['std_price_30'] / df_new['std_price_365']
+    # standard deviation of volumes
+    df_new['std_volume_5'] = df['Volume'].rolling(5).std().shift(1)
+    df_new['std_volume_30'] = df['Volume'].rolling(21).std().shift(1)
+    df_new['std_volume_365'] = df['Volume'].rolling(252).std().shift(1)
+    df_new['ratio_std_volume_5_30'] = df_new['std_volume_5'] / df_new['std_volume_30']
+    df_new['ratio_std_volume_5_365'] = df_new['std_volume_5'] / df_new['std_volume_365']
+    df_new['ratio_std_volume_30_365'] = df_new['std_volume_30'] / df_new['std_volume_365']
+    # # return
+    df_new['return_1'] = ((df['Close'] - df['Close'].shift(1)) / df['Close'].shift(1)).shift(1)
+    df_new['return_5'] = ((df['Close'] - df['Close'].shift(5)) / df['Close'].shift(5)).shift(1)
+    df_new['return_30'] = ((df['Close'] - df['Close'].shift(21)) / df['Close'].shift(21)).shift(1)
+    df_new['return_365'] = ((df['Close'] - df['Close'].shift(252)) / df['Close'].shift(252)).shift(1)
+    df_new['moving_avg_5'] = df_new['return_1'].rolling(5).mean().shift(1)
+    df_new['moving_avg_30'] = df_new['return_1'].rolling(21).mean().shift(1)
+    df_new['moving_avg_365'] = df_new['return_1'].rolling(252).mean().shift(1)
+    # the target
+    df_new['close'] = df['Close']
+    df_new = df_new.dropna(axis=0)
+    return df_new
+
+# load data, generate features, and label the function as generate features
+
+data_raw = pd.read_csv("data_file.csv", index_col='Date')
+data = generate_features(data_raw)
+
+# construct the training set from 1988-2018 and the testing set from 2019
+
+start_train = '1988-01-01'
+end_train = '2018-12-31'
+
+start_test = '2019-01-01'
+end_test = '2019-12-31'
+
+data_train = data.lco[start_train:end_train]
+X_train = data_train.drop('close', axis=1).values
+y_train = data_train['close'].values
+
+data_test = data.loc[start_test:end_test]
+X_test = data_test.drop('close', axis=1).values
+y_test = data_test['close'].values
