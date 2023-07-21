4e5b75d6e16a4522e1727a235d5904ceb107ba10
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-21 12:33:15 -0400

    Add files via upload

--- Ch 8/stock_prediction_ch_8.py
+++ Ch 8/stock_prediction_ch_8.py
@@ -189,3 +189,21 @@ def run(hparams, logdir):
         tf.summary.scalar('mean_squared_error', mse, step=1)
         tf.summary.scalar('r2', r2, step=1)
 
+# now train the model for each different combo of the hyperparameters in a gridsearch manner
+
+session_num = 0
+
+for hidden in HP_HIDDEN.domain.values:
+    for epochs in HP_EPOCHS.domain.values:
+        for learning_rate in tf.linspace(HP_LEARNING_RATE.domain.min_value, HP_LEARNING_RATE.domain.max_value, 5):
+            hparams = {
+                HP_HIDDEN: hidden,
+                HP_EPOCHS: epochs,
+                HP_LEARNING_RATE: float("%.2f"%float(learning_rate)),
+            }
+            run_name = "run-%d" % session_num
+            print('--- Starting trial: %s' % run_name)
+            print({h.name: hparams[h] for h in hparams})
+            run(hparams, 'logs/hparam_tuning/' + run_name)
+            session_num += 1
+
