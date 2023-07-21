10ce456781312fc5d63eccbe00f562eac8166e9c
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-21 16:26:10 -0400

    Add files via upload

--- Ch 9/exploring_nlp_ch_9.py
+++ Ch 9/exploring_nlp_ch_9.py
@@ -44,3 +44,8 @@ nlp = spacy.load('en_core_web_sm')
 tokens2 = nlp(sent2)
 print([token.text for token in tokens2])
 
+from nltk.tokenize import sent_tokenize
+print(sent_tokenize(sent))
+
+# two sentences are returned, as there are two sentences as the input text
+
