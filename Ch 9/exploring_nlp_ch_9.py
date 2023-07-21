5278358de11d14f685ef1653e46415ef0d4d1613
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-21 14:29:49 -0400

    Add files via upload

---
+++ Ch 9/exploring_nlp_ch_9.py
@@ -0,0 +1,8 @@
+"""
+Touring popular NLP libraries and picking up NLP basics
+
+Liu, Yuxi (Hayden). Python Machine Learning By Example: Build intelligent systems using Python, TensorFlow 2, PyTorch, and scikit-learn, 3rd Edition (p. 285). Packt Publishing. Kindle Edition. 
+"""
+
+import nltk
+nltk.download()
