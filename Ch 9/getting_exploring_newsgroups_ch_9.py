a8fe002257d7acbed445a7649e854ab74d317e4e
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-22 12:23:40 -0400

    Add files via upload

---
+++ Ch 9/getting_exploring_newsgroups_ch_9.py
@@ -0,0 +1,8 @@
+"""
+Getting the newsgroups data
+
+Liu, Yuxi (Hayden). Python Machine Learning By Example: Build intelligent systems using Python, TensorFlow 2, PyTorch, and scikit-learn, 3rd Edition (p. 294). Packt Publishing. Kindle Edition. 
+"""
+
+from sklearn.datasets import fetch_20newsgroups
+
