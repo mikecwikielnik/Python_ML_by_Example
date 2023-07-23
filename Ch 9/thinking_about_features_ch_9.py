1feec67a7d5d510147ce65dd94856690f6aa7942
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-23 19:59:00 -0400

    Add files via upload

--- Ch 9/thinking_about_features_ch_9.py
+++ Ch 9/thinking_about_features_ch_9.py
@@ -4,13 +4,17 @@ Thinking about features for text data
 Liu, Yuxi (Hayden). Python Machine Learning By Example: Build intelligent systems using Python, TensorFlow 2, PyTorch, and scikit-learn, 3rd Edition (p. 301). Packt Publishing. Kindle Edition. 
 """
 
-# from sklearn.datasets import fetch_20newsgroups
+from sklearn.datasets import fetch_20newsgroups
 
-# groups = fetch_20newsgroups()
+groups = fetch_20newsgroups()
 
 from sklearn.feature_extraction.text import CountVectorizer
 
 # first, initialize the count vectorizer w/ 500 top features (500 most frequent tokens):
 
 count_vector = CountVectorizer(stop_words="english", max_features=500)
+
+# use it to fit on the raw text data as follows:
+
+data_count = count_vector.fit_transform(groups.data)
 
