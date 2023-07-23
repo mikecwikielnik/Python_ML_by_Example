8e356c65d663fe31550f2364c744a1e74e9a7e9a
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-23 19:41:02 -0400

    Add files via upload

---
+++ Ch 9/thinking_about_features_ch_9.py
@@ -0,0 +1,12 @@
+"""
+Thinking about features for text data
+
+Liu, Yuxi (Hayden). Python Machine Learning By Example: Build intelligent systems using Python, TensorFlow 2, PyTorch, and scikit-learn, 3rd Edition (p. 301). Packt Publishing. Kindle Edition. 
+"""
+
+# from sklearn.datasets import fetch_20newsgroups
+
+# groups = fetch_20newsgroups()
+
+from sklearn.feature_extraction.text import CountVectorizer
+
