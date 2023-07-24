5a692da4e1b615e803f018720a3804a32a871a85
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-24 14:53:33 -0400

    Add files via upload

--- Ch 9/tSNE_ch_9.py
+++ Ch 9/tSNE_ch_9.py
@@ -51,3 +51,21 @@ categories_5 = ['comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.har
                 'comp.windows.x']
 groups_5 = fetch_20newsgroups(categories = categories_5)
 
+
+
+count_vector_sw = CountVectorizer(stop_words="english", max_features=500)
+
+data_cleaned = []
+
+for doc in groups_5.data:
+    doc = doc.lower()
+    doc_cleaned = ' '.join(lemmatizer.lemmatize(word) for word in doc.split() if word.isalpha() and word not in all_names)
+    data_cleaned.append(doc_cleaned)
+
+data_cleaned_count_5 = count_vector_sw.fit_transform(data_cleaned)
+
+data_tsne = tsne_model.fit_transform(data_cleaned_count_5.toarray())
+
+plt.scatter(data_tsne[:, 0], data_tsne[:, 1], c=groups_5.target)
+
+plt.show()
