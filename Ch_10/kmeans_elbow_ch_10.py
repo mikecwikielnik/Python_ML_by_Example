5ac5d33c699b92953fbde3f657f2d709f9ef62f2
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-25 15:47:13 -0400

    Add files via upload

--- Ch_10/kmeans_elbow_ch_10.py
+++ Ch_10/kmeans_elbow_ch_10.py
@@ -9,6 +9,12 @@ from sklearn.cluster import KMeans
 import numpy as np
 from matplotlib import pyplot as plt
 
+# sse = sum of squared errors
+# sse is computed as the sum of the squared distances
+# from individual samples in the cluster TO the centroid
+
+# ex: we perform k-means clusterting under different values of k on the iris data
+
 iris = datasets.load_iris()
 X = iris.data
 y = iris.target
@@ -16,9 +22,22 @@ y = iris.target
 k_list = list(range(1, 7))
 sse_list = [0] * len(k_list)
 
-# sse = sum of squared errors
-# sse is computed as the sum of the squared distances
-# from individual samples in the cluster TO the centroid
+# we use the whole feature space and k ranges 1:6
+# we then train individual models and record the resulting SSE
 
-# ex: we perform k-means clusterting under different values of k on the iris data
+for k_ind, k in enumerate(k_list):
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit(X)
+    clusters = kmeans.labels_
+    centroids = kmeans.cluster_centers_
+
+    sse = 0
+    for i in range(k):
+        cluster_i = np.where(clusters == i)
+
+        sse += np.linalg.norm(X[cluster_i] - centroids[i])
+
+    print('k={}, SSE={}'.format(k, sse))
+    sse_list[k_ind] = sse
+
 
