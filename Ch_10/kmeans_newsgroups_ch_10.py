44d9185595ced26b88ebe5998509d626f24cc6ae
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-25 16:15:04 -0400

    Add files via upload

---
+++ Ch_10/kmeans_newsgroups_ch_10.py
@@ -0,0 +1,34 @@
+"""
+Clustering newsgroups data using k-means
+
+Liu, Yuxi (Hayden). Python Machine Learning By Example: Build intelligent systems using Python, TensorFlow 2, PyTorch, and scikit-learn, 3rd Edition (p. 333). Packt Publishing. Kindle Edition. 
+"""
+
+from sklearn.datasets import fetch_20newsgroups
+
+categories = [
+    'alt.atheism',
+    'talk.religion.misc',
+    'comp.graphics',
+    'sci.space'
+]
+
+groups = fetch_20newsgroups(subset='all', categories=categories)
+
+labels = groups.target
+label_names = groups.target_names
+
+from nltk.corpus import names
+all_names = set(names.words())
+
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+data_cleaned = []
+
+for doc in groups.data:
+    doc = doc.lower()
+    doc_cleaned = ' '.join(lemmatizer.lemmatize(word) for word in doc.split() if word.isalpha() and word not in all_names)
+    data_cleaned.append(doc_cleaned)
+
+    
