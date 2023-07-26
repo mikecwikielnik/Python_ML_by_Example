0b9cbedd716994517301217b687b043731c89b8c
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-26 13:42:30 -0400

    Add files via upload

---
+++ Ch_11/imputation_ch_11.py
@@ -0,0 +1,9 @@
+"""
+Dealing with missing data
+
+Liu, Yuxi (Hayden). Python Machine Learning By Example: Build intelligent systems using Python, TensorFlow 2, PyTorch, and scikit-learn, 3rd Edition (p. 350). Packt Publishing. Kindle Edition. 
+"""
+
+import numpy as np
+from sklearn.impute import SimpleImputer
+
