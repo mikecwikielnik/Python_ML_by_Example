abcb4f7ede4a52b2641fa938891a1cbb2770fe91
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-26 11:32:27 -0400

    Add files via upload

---
+++ Ch_10/nmf_newsgroups_ch_10.py
@@ -0,0 +1,39 @@
+"""
+Topic modeling using NMF
+
+Liu, Yuxi (Hayden). Python Machine Learning By Example: Build intelligent systems using Python, TensorFlow 2, PyTorch, and scikit-learn, 3rd Edition (p. 338). Packt Publishing. Kindle Edition. 
+"""
+
+
+from sklearn.datasets import fetch_20newsgroups
+
+categories = [
+    'alt.atheism',
+    'talk.religion.misc',
+    'comp.graphics',
+    'sci.space',
+]
+
+
+groups = fetch_20newsgroups(subset='all', categories=categories)
+
+from nltk.corpus import names
+all_names = set(names.words())
+
+
+
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+data_cleaned = []
+
+for doc in groups.data:
+    doc = doc.lower()
+    doc_cleaned = ' '.join(lemmatizer.lemmatize(word) for word in doc.split() if word.isalpha() and word not in all_names)
+    data_cleaned.append(doc_cleaned)
+
+
+from sklearn.feature_extraction.text import CountVectorizer
+count_vector = CountVectorizer(stop_words="english", max_features=None, max_df=0.5, min_df=2)
+
+data = count_vector.fit_transform(data_cleaned)
