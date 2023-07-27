5700dcc3833b578946d687850eb80463f6c1d2c6
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-07-27 16:50:42 -0400

    Add files via upload

--- Ch_11/generic_feature_engineering_ch_11.py
+++ Ch_11/generic_feature_engineering_ch_11.py
@@ -11,3 +11,16 @@ X = [[4], [1], [3], [0]]
 binarizer = Binarizer(threshold=2.9)
 X_new = binarizer.fit_transform(X)
 print(X_new)
+
+# polynomial transformation 
+
+from sklearn.preprocessing import PolynomialFeatures
+
+X = [[2, 4],
+     [1, 3],
+     [3, 2],
+     [0, 3]]
+poly = PolynomialFeatures(degree=2)
+X_new = poly.fit_transform(X)
+print(X_new)
+
