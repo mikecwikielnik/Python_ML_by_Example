54aeacf34e6660193d649ba24b5de8db748e2552
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-08-01 15:48:13 -0400

    Add files via upload

--- Ch_12/classify_image_augmentation_ch_12.py
+++ Ch_12/classify_image_augmentation_ch_12.py
@@ -98,3 +98,15 @@ print(X_train.shape)
 
 # we only use 500 samples for training
 
+# we build the CNN model using the Keras Sequential API
+
+from keras import datasets, layers, models, losses
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
