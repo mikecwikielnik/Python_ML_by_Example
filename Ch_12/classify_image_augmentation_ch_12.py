ebfadac6164751a4b9aa2d09a216350e87756c65
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2023-08-01 15:00:14 -0400

    Add files via upload

--- Ch_12/classify_image_augmentation_ch_12.py
+++ Ch_12/classify_image_augmentation_ch_12.py
@@ -7,6 +7,7 @@ Liu, Yuxi (Hayden). Python Machine Learning By Example: Build intelligent system
 import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
+import os
 
 fashion_mnist = tf.keras.datasets.fashion_mnist
 
@@ -18,3 +19,34 @@ class_names = ['t-shirt/top', 'trouser','pullover', 'dress', 'coat',
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
+# we will create manipulated images using this generator.
+# first, we develop a utility fn to generate images
+# given an augmented image generator and display them
+
+from keras.preprocessing.image import load_img
+
+def generate_plot_pics(datagen, original_img, save_prefix):
+    folder = 'aug_images'
+    i = 0
+    for batch in datagen.flow(original_img.reshape((-1, 28, 28, 1)),
+                              batch_size=1,
+                              save_to_dir = folder,
+                              save_prefix = save_prefix,
+                              save_format = 'jpeg'):
+        i += 1
+        if i > 2:
+            break
+    plt.subplot(2, 2, 1, xticks=[], yticks=[])
+    plt.imshow(original_img)
+    plt.title("Original")
+    i = 1
+    for file in os.listdif(folder):
+        if file.startswith(save_prefix):
+            plt.subplot(2, 2, i + 1, xticks=[], yticks=[])
+            aug_img = load_img(folder + "/" + file)
+            plt.imshow(aug_img)
+            plt.title(f"Augmented {i}")
+            i += 1
+    plt.show()
+
+    
